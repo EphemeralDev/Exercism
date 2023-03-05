207efec563c4f339b7706ecaa5f1ae50dac80607
EphemeralDev <>
2023-03-05 15:11:16 -0500

    Initial working solution

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,30 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut prime_count = 0;
+    let mut candidate = 2;
+
+    loop {
+        if prime_count == n {
+            return candidate;
+        }
+        candidate += 1;
+        if primality_test(candidate) {
+            prime_count += 1;
+        }
+    }
+}
+
+pub fn primality_test(n: u32) -> bool {
+    if n == 2 || n == 3 {
+        return true;
+    }
+    if n <= 1 || n % 2 == 0 || n % 3 == 0 {
+        return false;
+    }
+
+    for i in (5..=(n as f32).sqrt() as u32).step_by(6) {
+        if n % i == 0 || n % (i + 2) == 0 {
+            return false;
+        }
+    }
+    true
 }
