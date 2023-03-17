4a7ff51abe37c1c4c4b02c5f0bf062d6eae23bb6
EphemeralDev <>
2023-03-17 00:56:29 -0400

    Working solution to rust/assembly-line.

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,7 +1,19 @@
+const BASE_HOURLY: f64 = 221.0;
+const MINUTES_IN_HOUR: u32 = 60;
+
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {speed}")
+    BASE_HOURLY * speed as f64 * success_rate(speed)
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {speed}")
+    production_rate_per_hour(speed) as u32 / MINUTES_IN_HOUR
+}
+
+pub fn success_rate(speed: u8) -> f64 {
+    match speed {
+        0 => 0.0,
+        1..=4 => 1.0,
+        5..=8 => 0.90,
+        9..=u8::MAX => 0.77,
+    }
 }
