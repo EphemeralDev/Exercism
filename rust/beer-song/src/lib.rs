600b20a8c11346c95050b7b9a3ab8060a6a70311
EphemeralDev <115334775+EphemeralDev@users.noreply.github.com>
2023-04-09 00:36:20 -0400

    Working solution to rust/beer-song

---
+++ rust/beer-song/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn verse(n: u32) -> String {
+    match n {
+
+        0 => format!("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        
+        1 => format!("{} bottle of beer on the wall, {} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", n, n),
+        
+        2 => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle of beer on the wall.\n", n, n, n-1),
+        
+        _ => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n-1),}
+}
+
+pub fn sing(start: u32, end: u32) -> String {
+    let mut song = String::new();
+    for i in (end..=start).rev() {
+        song += "\n";
+        song += &verse(i);
+    }
+    song.trim_start().to_string()
+}
