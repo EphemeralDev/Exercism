18490f022614b89675b10715771d5ebb1c2554c1
EphemeralDev <115334775+EphemeralDev@users.noreply.github.com>
2023-04-14 00:42:00 -0400

    Updated solution to rust/leap

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,8 +1,9 @@
 pub fn is_leap_year(year: u64) -> bool {
     if year % 400 == 0 {
-        return true;
+        true
     } else if year % 100 == 0 {
-        return false;
+        false
+    } else {
+        year % 4 == 0
     }
-    year % 4 == 0
 }
