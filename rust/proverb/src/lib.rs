42380612af54acb8692b5613e2a9026e8c006411
EphemeralDev <115334775+EphemeralDev@users.noreply.github.com>
2023-05-02 00:47:13 -0400

    Updated solution to rust/proverb

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,15 +1,13 @@
-pub fn build_proverb(list: &[&str]) -> String {
-    let mut proverb = String::new();
+use std::iter::once;
 
+pub fn build_proverb(list: &[&str]) -> String {
     match list.len() {
-        1 => format!("And all for the want of a {}.", list[0]),
-        2.. => {
-            for i in 0..list.len() - 1 {
-                proverb +=
-                    format!("For want of a {} the {} was lost.\n", list[i], list[i + 1]).as_str();
-            }
-            proverb + format!("And all for the want of a {}.", list[0]).as_str()
-        }
-        _ => proverb,
+        1.. => list
+            .windows(2)
+            .map(|word| format!("For want of a {} the {} was lost.\n", word[0], word[1]))
+            .chain(once(format!("And all for the want of a {}.", list[0])))
+            .collect(),
+
+        _ => String::new(),
     }
 }
